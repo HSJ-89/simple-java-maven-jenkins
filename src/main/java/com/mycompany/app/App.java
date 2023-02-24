a830d87b68f638cb5f9186877a7707358158b97c
harsha.sj89 <harsha.sj89@gmail.com>
2023-02-24 23:57:54 +0530

    Test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World from HSJ!";
+    private final String message = "Hello from HSJ!";
 
     public App() {}
 
